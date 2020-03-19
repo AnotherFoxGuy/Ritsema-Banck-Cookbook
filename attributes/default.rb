0c03380304f428c75ccd6918866c59c5e6074299
Edgar <Edgar@AnotherFoxGuy.com>
2020-03-19 19:25:12 +0100

    :tada: LDAP && migration

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['openldap']['basedn'] = 'dc=ritsema-banck, dc=frl'
+default['openldap']['server'] = 'ritsema-banck.frl'
+default['openldap']['rootpw'] = '{SSHA}6BjlvtSbVCL88li8IorkqMSofkLio58/'
+default['openldap']['rootpwplain'] = 'secretsauce' # required for the shitty ldapadd tool
--- recipes/ldap.rb
+++ recipes/ldap.rb
@@ -3,4 +3,28 @@
 # Recipe:: ldap
 #
 
+include_recipe 'openldap::default'
+
+template '/etc/ldap/ldap.conf' do
+  source 'ldap.conf'
+  mode '0640'
+  owner node['openldap']['system_acct']
+  group node['openldap']['system_group']
+end
+
+service 'slapd' do
+  action :restart
+end
+
+execute 'slapadd' do
+  #command 'slapadd -l /tmp/dump.ldif'
+  command "ldapadd -D \"cn=admin,dc=ritsema-banck, dc=frl\" -w \"#{node['openldap']['rootpwplain']}\" -f /tmp/dump.ldif"
+  creates '/var/lib/slapd/uid.bdb'
+  action :nothing
+end
+
+template '/tmp/dump.ldif' do
+  source 'dump.ldif'
+  notifies :run, 'execute[slapadd]', :immediately
+end
 
