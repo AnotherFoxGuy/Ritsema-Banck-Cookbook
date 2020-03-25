4fdfdedec4c671f9672b193d66360ffd0549bd41
Edgar <Edgar@AnotherFoxGuy.com>
2020-03-25 10:29:02 +0100

    :bug: Open ssh port in firewall

--- recipes/security.rb
+++ recipes/security.rb
@@ -7,6 +7,12 @@
 
 firewall 'default'
 
+# open standard ssh port
+firewall_rule 'ssh' do
+  port     22
+  command  :allow
+end
+
 # open multiple ports for http/https, note that the protocol
 # attribute is required when using ports
 firewall_rule 'http/https' do
